c1555e69f1d473fc8f64c2a29bec2447510acecf
hongcui <hong1.cui@gmail.com>
2018-12-12 12:14:25 -0700

    Revert "added user and ontology to the site url"
    This reverts commit 4529d689165e5a690ba338191b3c12e96651ccfc.

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -8,8 +8,6 @@ import WebProtege from './components/Pages/web_protege.vue';
 import TermsWiki from './components/Pages/terms_wiki.vue';
 
 const routes = [
-    //Heng's code
-    /*
     {
         path: '/add2ontology/public/:term',
         component: Home,
@@ -32,32 +30,6 @@ const routes = [
     },
     {
         path: '/add2ontology/public/terms-wiki/:term',
-        component: TermsWiki,
-        name: 'terms-wiki'
-    },*/
-    //Hong's attempt
-     {
-        path: '/add2ontology/public/:user/:ontology/:term',
-        component: Home,
-        name: 'home'
-    },
-    {
-        path: '/add2ontology/public/wizard/:user/:ontology/:term',
-        component: Wizard,
-        name: 'wizard'
-    },
-    {
-        path: '/add2ontology/public/quick-form/:user/:ontology/:term',
-        component: QuickForm,
-        name: 'quick-form'
-    },
-    {
-        path: '/add2ontology/public/web-protege/:user/:ontology/:term',
-        component: WebProtege,
-        name: 'web-protege'
-    },
-    {
-        path: '/add2ontology/public/terms-wiki/:user/:ontology/:term',
         component: TermsWiki,
         name: 'terms-wiki'
     },
--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,11 @@
 //
 //Auth::routes();
 
-
-//Heng's code
-//Route::get('/{term}', 'HomeController@index')->name('home');
-//Route::get('/wizard/{term}', 'HomeController@wizard')->name('wizard');
-//Route::get('/quick-form/{term}', 'HomeController@quickForm')->name('quick-form');
-//Route::get('/web-protege/{term}', 'HomeController@webProtege')->name('web-protege');
-//Route::get('/terms-wiki/{term}', 'HomeController@termsWiki')->name('terms-wiki');
-
-//Hong's try
-Route::get('/{user}/{ontology}/{term}', 'HomeController@index')->name('home');
-Route::get('/wizard/{user}/{ontology}/{term}', 'HomeController@wizard')->name('wizard');
-Route::get('/quick-form/{user}/{ontology}/{term}', 'HomeController@quickForm')->name('quick-form');
-Route::get('/web-protege/{user}/{ontology}/{term}', 'HomeController@webProtege')->name('web-protege');
-Route::get('/terms-wiki/{user}/{ontology}/{term}', 'HomeController@termsWiki')->name('terms-wiki');
-
+Route::get('/{term}', 'HomeController@index')->name('home');
+Route::get('/wizard/{term}', 'HomeController@wizard')->name('wizard');
+Route::get('/quick-form/{term}', 'HomeController@quickForm')->name('quick-form');
+Route::get('/web-protege/{term}', 'HomeController@webProtege')->name('web-protege');
+Route::get('/terms-wiki/{term}', 'HomeController@termsWiki')->name('terms-wiki');
 //
 //Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
 //    Route::get('activity_log', 'ActivityLogController@index')->name('activity.index');
